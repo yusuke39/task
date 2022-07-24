9c0d36980e55cb5041f8356a160b34f4e691009e
yusuke39 <hiranoyusuke39@gmail.com>
2022-07-24 15:27:25 +0900

    :sparkles:CORSの設定&疎通確認用のAPIを作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :test , only: %i[index]
+    end
+  end
 end
